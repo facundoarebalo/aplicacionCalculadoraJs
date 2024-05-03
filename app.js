9d17a437887f000df950893a66767ffbbff65e58
Facundo Arebalo <facuar1589@gmail.com>
2024-05-03 16:28:59 -0300

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+const sumar = () => {
+    const forma = document.getElementById('forma')
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value)
+    if(isNaN(resultado))
+        resultado = 'La operación no incluye números';
+    document.getElementById('resultado').innerHTML = `El resultado es: ${resultado}`
+
+}
